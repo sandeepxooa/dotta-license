f26f56d069f8164a21ac729c10b73ffc6cec7248
Sandeep Patel <37658717+sandeepxooa@users.noreply.github.com>
2019-11-29 13:20:00 +0530

    Update truffle.js

--- dot-license-contracts/truffle.js
+++ dot-license-contracts/truffle.js
@@ -8,6 +8,12 @@ module.exports = {
       enabled: true,
       runs: 200
     }
-  }
+  },
+  
+  compilers: {
+    solc: {
+      version: "0.4.19" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ }
   
 };
